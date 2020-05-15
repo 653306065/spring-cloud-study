c3bed91821ce921a95f8c798ca1f94108961831a
fuqiang <fuqiang@DESKTOP-5FDRTHV>
2020-05-15 22:10:15 +0800

    增加zuul

--- consumer-service/src/main/java/org/study/spring/consumer/service/controller/GetDataController.java
+++ consumer-service/src/main/java/org/study/spring/consumer/service/controller/GetDataController.java
@@ -1,24 +1,17 @@
-package org.first.service.controller;
-
-import java.util.UUID;
+package org.study.spring.consumer.service.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @RequestMapping("/getData")
+@RestController
 public class GetDataController {
-	
+
 	@Value("${spring.application.name}")
 	private String name;
 
-	@GetMapping("getValue")
-	public String getValue() {
-		return UUID.randomUUID().toString();
-	}
-	
 	@GetMapping("/getName")
 	public String getName() {
 		return name;
